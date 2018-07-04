1df5dcc867bd0d9c9deaaa97b63c3e625ef358a2
gerardobudzovsky <gerardo.dav.bud@gmail.com>
2018-07-04 12:23:19 -0300

    Topic 01

---
+++ topic01/src/main/java/com/javabootcamp/app/App.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from the Java Bootcamp!" );
+    }
+}
